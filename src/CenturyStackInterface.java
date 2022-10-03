335790514cf4af75d014192df037a979be42e2d8
patybn3 <paty.nigro@gmail.com>
2022-10-03 11:59:28 -0400

    CenturyStack_Implemented.java is a file that will generate random numbers and insert them into an array, it will then remove the first element from an array of pre-set size of 100 elements. It will then replace the last element with a random number generated by the program.

--- src/CenturyStackInterface.java
+++ src/CenturyStackInterface.java
@@ -1,2 +1,9 @@
-public interface CenturyStackInterface {
+public interface CenturyStackInterface
+{
+    int ARRAYMAX = 100;
+
+    int push(int stackElements);
+
+    int pop(int num);
+
 }
--- src/CenturyStack_PatriciaAntlitz.java
+++ src/CenturyStack_PatriciaAntlitz.java
@@ -1,5 +1,13 @@
-public class CenturyStack_PatriciaAntlitz {
-    public static void main(String[] args) {
+public class CenturyStack_PatriciaAntlitz
+{
+    public static void main(String[] args)
+    {
+        int random;
+        CenturyStack_Implemented runProgram = new CenturyStack_Implemented();
 
+        random = (int)(Math.random() * 100 + 1);
+        //ARRAY EXAMPLE
+        runProgram.pushToArray();
+        runProgram.pop(random);
     }
 }

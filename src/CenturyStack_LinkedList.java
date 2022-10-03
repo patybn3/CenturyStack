bf8d691a871c2ab2e6f48f43371ea18ae4f24859
patybn3 <paty.nigro@gmail.com>
2022-10-03 12:02:34 -0400

    ArrayList and LinkedList files created. Implements Interface CenturyStackInterface.java

---
+++ src/CenturyStack_LinkedList.java
@@ -0,0 +1,14 @@
+public class CenturyStack_LinkedList implements CenturyStackInterface
+{
+    @Override
+    public int push(int stackElements)
+    {
+        return 0;
+    }
+
+    @Override
+    public int pop(int num)
+    {
+        return 0;
+    }
+}
